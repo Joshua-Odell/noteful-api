e9a737d61bfdcee73264b6ba219f5e7235d4c015
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-08-22 11:04:25 -0500

    Fixed DB connection

--- src/folders/router.js
+++ src/folders/router.js
@@ -6,8 +6,8 @@ const FoldersService = require('./service')
 const foldersRouter = express.Router()
 const jsonParser = express.json()
 
-const serializeUser = folder => ({
-    id: user.id,
+const serializeFolder = folder => ({
+    id: folder.id,
     name: xss(folder.name),
 })
 
@@ -15,13 +15,14 @@ foldersRouter
     .route('/')
     .get((req, res, next) => {
         const knexInstance = req.app.get('db')
-        console.log(knexInstance)
         FoldersService.getAllFolders(knexInstance)
             .then(folder => {
-                res.json(folder.map(serializeUser))
+                res.json(folder.map(serializeFolder))
             })
         .catch(next)
     })
+    // Error: insert into "noteful_folders" ("0", "1", "2", "3", "4", "5", "6", "7", "8") values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning * - column "0" of relation "noteful_folders" does not exist
+    // Information Sent name: "test"
     .post(jsonParser, (req, res, next) => {
         const { name } = req.body
         const newFolderName = name
@@ -31,17 +32,19 @@ foldersRouter
                 res
                     .status(201)
                     .location(path.posix.join(req.originalUrl, `/${folder.id}`))
-                    .json(serializeUser(folder))
+                    .json(serializeFser(folder))
             })
             .catch(next)
     })
 
 foldersRouter
     .route('/:folder_id')
+    //No notes are assigned to any folders
     .get((req, res, next) => {
         FoldersService.getById( req.app.get('db'), req.params.id)
             .catch(next)
     })
+    //Not an option in this app
     .delete((req, res, next) => {
         FoldersService.deleteFolder(
             req.app.get('db'),
--- src/notes/router.js
+++ src/notes/router.js
@@ -6,7 +6,7 @@ const NotesService = require('./service')
 const NotesRouter = express.Router()
 const jsonParser = express.json()
 
-const serializeUser = note => ({
+const serializeNote = note => ({
     id: note.id,
     name: xss(note.name),
     modified: xss(note.modified),
@@ -17,34 +17,36 @@ NotesRouter
     .route('/')
     .get((req, res, next) => {
         const knexInstance = req.app.get('db')
-        console.log(knexInstance)
         NotesService.getAllNotes(knexInstance)
             .then(folder => {
-                res.json(folder.map(serializeUser))
+                res.json(folder.map(serializeNote))
             })
         .catch(next)
     })
+    //error occuring "insert into "noteful_notes" ("name") values ($1) returning * - null value in column "content" violates not-null constraint"
+    // Information sent content: "testest" folderId: "2" name: "test"
     .post(jsonParser, (req, res, next) => {
         const { name } = req.body
         const newNote = { name } 
-        console.log(newNote);
 
         NotesService.insertNotes(req.app.get('db'), newNote)
             .then(notes => {
                 res
                     .status(201)
                     .location(path.posix.join(req.originalUrl, `/${notes.id}`))
-                    .json(serializeUser(notes))
+                    .json(serializeNote(notes))
             })
             .catch(next)
     })
 
 NotesRouter
     .route('/:note_id')
+    //Content is not displayed No get request is made
     .get((req, res, next) => {
         NotesService.getById( req.app.get('db'), req.params.id)
             .catch(next)
     })
+    //A error is occuring because a delete request does not send the id
     .delete((req, res, next) => {
         NotesService.deleteNotes(
             req.app.get('db'),
